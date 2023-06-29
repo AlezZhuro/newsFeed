ebf37241fccbf76f0b9ac9768ec2367ff4a84de0
alezzhuro <alezzhuro@gmail.com>
2023-06-29 19:42:04 +0300

    refactor: init react-native-gesture-handler lib;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import {Providers} from './providers';
 import {Routing} from './routes';
-import 'react-native-gesture-handler';
 
 function App(): JSX.Element {
   return (
--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import {PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import Toast from 'react-native-toast-message';
 
 import {store} from 'app/store';
 import {toastConfig} from 'shared';
 
-export const Providers: React.FC<React.PropsWithChildren> = ({children}) => {
-  return (
-    <Provider store={store}>
-      <PaperProvider>
-        <SafeAreaProvider>{children}</SafeAreaProvider>
-        <Toast config={toastConfig} />
-      </PaperProvider>
-    </Provider>
-  );
-};
+export const Providers = gestureHandlerRootHOC(
+  ({children}: React.PropsWithChildren) => {
+    return (
+      <Provider store={store}>
+        <PaperProvider>
+          <SafeAreaProvider>{children}</SafeAreaProvider>
+          <Toast config={toastConfig} />
+        </PaperProvider>
+      </Provider>
+    );
+  },
+);
