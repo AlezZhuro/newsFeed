fefe681b012630b6d21c6a9932970b2e81e8bb10
alezzhuro <alezzhuro@gmail.com>
2023-06-30 18:59:34 +0300

    feat: spleash screen with icon for ios version ;

--- ios/newsFeed/AppDelegate.mm
+++ ios/newsFeed/AppDelegate.mm
@@ -2,16 +2,23 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import "RNSplashScreen.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"newsFeed";
+  
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+bool didFinish=[super application:application didFinishLaunchingWithOptions:launchOptions];
+[RNSplashScreen show];
+  // return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return didFinish;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
--- src/screens/Start/index.tsx
+++ src/screens/Start/index.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SplashScreen from 'react-native-splash-screen';
 
 import {authModel} from 'entities';
 import {config, useAppDispatch, ScreenProps, Screens} from 'shared';
@@ -12,18 +13,24 @@ export const StartScreen: React.FC<
   const dispatch = useAppDispatch();
 
   const init = async () => {
-    const authHeaders = await AsyncStorage.getItem(
-      config.asyncStorage.headersKey,
-    );
-    if (authHeaders === null) {
-      navigation.replace(Screens.SIGN_IN);
-      return;
-    }
+    try {
+      const authHeaders = await AsyncStorage.getItem(
+        config.asyncStorage.headersKey,
+      );
+      if (authHeaders === null) {
+        navigation.replace(Screens.SIGN_IN);
+        return;
+      }
 
-    const {meta} = await dispatch(authModel.refreshUser());
+      const {meta} = await dispatch(authModel.refreshUser());
 
-    if (meta.requestStatus === 'fulfilled') {
-      navigation.replace(Screens.HOME);
+      if (meta.requestStatus === 'fulfilled') {
+        navigation.replace(Screens.HOME);
+        return;
+      }
+    } catch (error: any) {
+    } finally {
+      SplashScreen.hide();
     }
   };
 
