b28c84bf9951c81d7e5e02f7f47ce71defbbd6d7
alezzhuro <alezzhuro@gmail.com>
2023-07-03 16:23:13 +0300

    refactor: removed unused packages;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import {Providers} from './providers';
 import {Routing} from './routes';
-import {useNotification} from 'features';
 
 const App = (): JSX.Element => {
-  useNotification();
   return (
     <Providers>
       <Routing />
